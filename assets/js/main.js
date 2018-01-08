10407ae6c3b35bc8a933744e9f143f163a598a62
Katherine <kbd3ax@virginia.edu>
2018-01-07 19:00:38 -0500

    Create JS to control which example is showing w/ btns

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,27 @@
 (function() {
-	var ex1 = document.querySelector("#ex1");
-	ex1.style.border = "2px solid red";
+
+	// section: examples
+	var exBtn1,
+		exBtn2,
+		exBtn3,
+		btnContainer,
+		exampleContainer,
+		btns;
+
+	exBtn1 				= document.querySelector(".home-examples__btn--1");
+	exBtn2 				= document.querySelector(".home-examples__btn--2");
+	exBtn3 				= document.querySelector(".home-examples__btn--3");
+	btnContainer 		= document.querySelector(".home-examples__btns");
+	exampleContainer 	= document.querySelector(".home-examples__content");
+	btns 				= [exBtn1, exBtn2, exBtn3];
+
+	for (let btn of btns) {
+		btn.addEventListener("click", (e) => {
+			let num = Array.prototype.indexOf.call(btnContainer.children, e.target);
+			let percentage = -100 * (num / 3);
+			
+			exampleContainer.style.transform = `translateX(${percentage}%)`;
+		});
+	}
+	// end examples
 })();
